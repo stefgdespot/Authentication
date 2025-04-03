a4b7451f3fef2496a3f6dd79de798ba74c8a5f9a
Stefan <stefan.g@despotsoft.com>
2025-04-03 10:45:31 +0200

    changes

--- authentication/src/app/app.routes.ts
+++ authentication/src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { VerificationComponent } from './verification/verification.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
     {
         path: "login",
         component: LoginComponent
